ba4b1724be900e4e91f4a52930c684f66e823a95
Austin-program <pouchainaustin@gmail.com>
2026-02-20 16:42:59 -0500

    Commited Motorbike.java

---
+++ src/main/java/org/example/Motorbike.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.example.Vehicle;
+
+public class Motorbike extends Vehicle {
+
+    private boolean hasSidecar;
+    private enum handlebarType;
+    private double forkLength;
+
+    public Motorbike(int vin, String make, String model, int year, int mileage, double price, boolean hasSidecar, double forkLength) {
+        super(vin, make, model, year, mileage, price);
+        this.hasSidecar = hasSidecar;
+        this.forkLength = forkLength;
+    }
+    @Override
+    public String getCategory() {
+        return "Motorbike";
+    }
+}
