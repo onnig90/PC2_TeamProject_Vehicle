779921444ccfca10a016bec932bcb0c46863b2b0
Austin-program <pouchainaustin@gmail.com>
2026-02-20 16:10:57 -0500

    Im commiting Car and Vehicle classes

---
+++ src/main/java/Car.java
@@ -0,0 +1,22 @@
+public class Car extends Vehicle {
+
+    private int doorCount;
+    private double trunkCapacity;
+    private boolean hasAirConditioning;
+
+    public Car(int vin, String make, String model, int year,
+               int mileage, double price, int doorsCount) {
+        super(vin, make, model, year, mileage, price);
+        this.doorCount = doorCount;
+        this.trunkCapacity = trunkCapacity;
+        this.hasAirConditioning = hasAirConditioning;
+
+    }
+    @Override
+    public String getCategory() {
+        return "Car";
+    }
+    public void openTrunk() {
+        System.out.println("Trunk opened. Capacity: " + trunkCapacity + " cubic feet");
+    }
+}
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,26 @@
+public abstract class Vehicle {
+
+    protected int vin;
+    protected String make;
+    protected String model;
+    protected int year;
+    protected int mileage;
+    protected double price;
+    protected boolean available;
+
+    public Vehicle(int vin, String make, String model, int year,
+                   int mileage, double price) {
+        this.vin = vin;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.price = price;
+        this.available = true;
+
+    }
+    public void markSold() {
+        available = false;
+    }
+    public abstract String getCategory();
+}
