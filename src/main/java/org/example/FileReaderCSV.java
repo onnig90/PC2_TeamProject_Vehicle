0f2b34e6077490e2a8c3ba0a0042e85965d7e79a
Onnig Ovanessian <onnig.ovanessian@gmail.com>
2026-02-23 20:04:34 -0500

    Csv reader + csv data

--- src/main/java/org/example/FileReaderCSV.java
+++ src/main/java/org/example/FileReaderCSV.java
@@ -1,24 +1,38 @@
 package org.example;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public final class FileReaderCSV {
-    private static final String csvFile = "data.csv"; // Path to your file
+    private static final String carCsvFile = "src/main/resources/CSV_Car.csv";
+    private static final String motorbikeCsvFile = "src/main/resources/CSV_Motorbike.csv";
     private static final String csvSplitBy = ",";
 
-    public static void LoadFromCSV(){
+    private static ArrayList<String[]> LoadFromCSV(String csvPath) {
+        ArrayList<String[]> carValuesList = new ArrayList<>();
         String line = "";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            while ((line = br.readLine()) != null) {
-                // Split line by comma
-                String[] data = line.split(csvSplitBy);
+        try (BufferedReader br = new BufferedReader(new FileReader(csvPath))) {
+            // Get headers
+            line = br.readLine();
 
-                // Example: Print the first two columns
-                System.out.println("Column 1: " + data[0] + ", Column 2: " + data[1]);
+            // Get values
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(csvSplitBy);
+                carValuesList.add(data);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return carValuesList;
+    }
+
+    public static ArrayList<String[]> LoadCarCSV() {
+        return LoadFromCSV(carCsvFile);
+    }
+
+    public static ArrayList<String[]> LoadMotorbikeCSV() {
+        return LoadFromCSV(motorbikeCsvFile);
     }
 }
